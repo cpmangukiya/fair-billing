fca136fd1e506e48f0976fcbb965b21f5d7f758e
cpmangukiya <chintanpmangukiya@gmail.com>
2021-05-06 15:33:57 +0530

    Error message(s) updated

--- src/main/java/FairBilling.java
+++ src/main/java/FairBilling.java
@@ -28,7 +28,7 @@ public class FairBilling {
     public static void main(String[] args) {
         try {
             if (args == null || args.length != 1) {
-                System.out.println("Desired input parameters could not be found. Please refer readme.md for more details !");
+                System.out.println("Error! Desired input parameters could not be found. Please refer readme.md for more details.");
                 return;
             }
 
